68a57bcf278cd2a157226d3b8f9419e181530e4f
Leonard Lee <Leonard133@users.noreply.github.com>
2021-05-10 15:59:01 +0800

    some fixes

--- config/config.php
+++ config/config.php
@@ -4,5 +4,30 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    /**  Available Packages
+            2. 'Laravel UI',
+            3. ' Jetstream',
+            4. 'Fortify',
+            5. 'Livewire',
+            6. 'Passport',
+            7. 'Excel',
+            8. 'Blueprint',
+            9. 'Debugbar',
+            10. 'Telescope',
+            11. 'Spatie Permission',
+            12. 'Datatable',
+            13. 'Horizon',
+            14. 'Backup',
+            15. 'Slack Notification',
+            16. 'Notification Channel',
+            17. 'Captcha',
+            18. 'QRCode',
+            19. 'Intervention Image',
+            20. 'Multi Tenancy',
+            21. 'Activity Log',
+            22. 'Tailwind CSS',
+            23. 'Tailwind UI',
+            24. 'Alpine.js'
+     */
+    'defaultPackages' => [2,3]
 ];
--- src/Console/PackageCommand.php
+++ src/Console/PackageCommand.php
@@ -3,9 +3,13 @@
 namespace Leonard133\Lazy\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class PackageCommand extends Command
 {
+
+    public $packages = [];
     /**
      * The name and signature of the console command.
      *
@@ -37,34 +41,164 @@ class PackageCommand extends Command
      */
     public function handle()
     {
-        $packages = [
+        $this->packages = [
             'Default Setup',
             'Laravel UI',
-            'Laravel Jetstream',
-            'Laravel Fortify',
-            'Laravel Livewire',
-            'Laravel Passport',
-            'Laravel Excel',
-            'Laravel Blueprint',
-            'Laravel Debugbar',
-            'Laravel Telescope',
-            'Laravel Spatie Permission',
-            'Laravel Datatable',
-            'Laravel Horizon',
-            'Laravel Backup',
-            'Laravel Slack Notification',
-            'Laravel Notification Channel',
-            'Laravel Captcha',
-            'Laravel QRCode',
-            'Laravel Intervention Image',
-            'Laravel Multi Tenancy',
-            'Laravel Activity Log',
+            'Jetstream',
+            'Fortify',
+            'Livewire',
+            'Passport',
+            'Excel',
+            'Blueprint',
+            'Debugbar',
+            'Telescope',
+            'Spatie Permission',
+            'Datatable',
+            'Horizon',
+            'Backup',
+            'Slack Notification',
+            'Notification Channel',
+            'Captcha',
+            'QRCode',
+            'Intervention Image',
+            'Multi Tenancy',
+            'Activity Log',
             'Tailwind CSS',
             'Tailwind UI',
             'Alpine.js'
         ];
-        $choices = $this->choice('Please choose the packages you would like to install (You may use "," for multiple choice)', $packages, 0, null, true);
-        dd($choices);
+        $choices = $this->choice('Please choose the packages you would like to install (You may use "," for multiple choice)', $this->packages, 0, null, true);
+        if (count($choices) > 0) {
+            $extra = 3;
+            ProgressBar::setFormatDefinition('custom', ' %current%/%max% [%bar%] %message%');
+            $bar = $this->output->createProgressBar(count($choices) + $extra);
+            $bar->setOverwrite(false);
+            $bar->setFormat('custom');
+            $bar->setMessage('Starting...');
+            $bar->start();
+
+            $this->progressPackage($bar, $choices);
+
+            $bar->setMessage('All packages complete installed. Happy Coding~');
+            $bar->finish();
+
+            $this->callSilent('migrate');
+            exec('composer dump-autoload -o --quiet');
+            $this->callSilent('cache:clear');
+            $this->callSilent('config:clear');
+            $this->callSilent('route:clear');
+            $this->newLine();
+        }
         return 0;
+    }
+
+    public function progressPackage($bar, $choices)
+    {
+        if (in_array('Default Setup', $choices))
+            $this->defaultSetup($bar);
+        if (in_array('Debugbar', $choices))
+            $this->setupDebugbar($bar);
+        if (in_array('Telescope', $choices))
+            $this->setupTelescope($bar);
+    }
+
+    public function setupDebugbar($bar)
+    {
+        if (!file_exists(config_path('debugbar.php'))) {
+            sleep(1);
+            $bar->setMessage('Installing debugbar...');
+            exec('composer require barryvdh/laravel-debugbar --prefer-stable --dev --quiet');
+            $bar->advance();
+            sleep(1);
+            $bar->setMessage('Configuring debugbar...');
+            copy(base_path('vendor/barryvdh/laravel-debugbar/config/debugbar.php'), config_path('debugbar.php'));
+            file_put_contents(config_path('debugbar.php'), str_replace('false, // Display Laravel authentication status', 'true, // Display Laravel authentication status', file_get_contents(config_path('debugbar.php'))));
+            $bar->advance();
+            sleep(1);
+            $bar->setMessage('Debugbar setup completed');
+            $bar->advance();
+            sleep(1);
+        } elseif (file_exists(config_path('debugbar.php'))) {
+            $bar->setMessage('Debugbar has been installed. Skipping debugbar setup');
+            $bar->advance(3);
+            sleep(1);
+        }
+    }
+
+    public function setupTelescope($bar)
+    {
+        if (!file_exists(config_path('telescope.php'))) {
+            sleep(1);
+            $bar->setMessage('Installing telescope...');
+            exec('composer require laravel/telescope --prefer-stable --dev --quiet');
+            $bar->advance();
+            sleep(1);
+            $bar->setMessage('Configuring telescope...');
+            copy(base_path('vendor/laravel/telescope/config/telescope.php'), config_path('telescope.php'));
+            \File::copyDirectory(base_path('vendor/laravel/telescope/public'), public_path('vendor/telescope'));
+            copy(base_path('vendor/leonard133/lazy/stubs/providers/TelescopeServiceProvider.stub'), app_path('Providers/TelescopeServiceProvider.php'));
+            $this->registerTelescopeServiceProvider();
+            $bar->advance();
+            sleep(1);
+            $bar->setMessage('Telescope setup completed');
+            $bar->advance();
+            sleep(1);
+        } elseif (file_exists(config_path('telescope.php'))) {
+            $bar->setMessage('Telescope has been installed. Skipping debugbar setup');
+            $bar->advance(3);
+            sleep(1);
+        }
+    }
+
+    public function defaultSetup($bar)
+    {
+        $default = config('lazy.defaultPackages');
+    }
+
+    protected function registerTelescopeServiceProvider()
+    {
+        $namespace = \Str::replaceLast('\\', '', $this->laravel->getNamespace());
+
+        $appConfig = file_get_contents(config_path('app.php'));
+
+        if (\Str::contains($appConfig, $namespace . '\\Providers\\TelescopeServiceProvider::class')) {
+            return;
+        }
+
+        $lineEndingCount = [
+            "\r\n" => substr_count($appConfig, "\r\n"),
+            "\r" => substr_count($appConfig, "\r"),
+            "\n" => substr_count($appConfig, "\n"),
+        ];
+
+        $eol = array_keys($lineEndingCount, max($lineEndingCount))[0];
+
+        file_put_contents(config_path('app.php'), str_replace(
+            "{$namespace}\\Providers\RouteServiceProvider::class," . $eol,
+            "{$namespace}\\Providers\RouteServiceProvider::class," . $eol . "        {$namespace}\Providers\TelescopeServiceProvider::class," . $eol,
+            $appConfig
+        ));
+
+        file_put_contents(app_path('Providers/TelescopeServiceProvider.php'), str_replace(
+            'namespace App\Providers;',
+            "namespace {$namespace}\Providers;",
+            file_get_contents(app_path('Providers/TelescopeServiceProvider.php'))
+        ));
+
+        $guards = array_keys(config('auth.guards'));
+        if(in_array('admin', $guards))
+        {
+            file_put_contents(app_path('Providers/TelescopeServiceProvider.php'), str_replace(
+                '{{ guard }}',
+                "!auth()->guard('admin')->guest()",
+                file_get_contents(app_path('Providers/TelescopeServiceProvider.php'))
+            ));
+        } else {
+            file_put_contents(app_path('Providers/TelescopeServiceProvider.php'), str_replace(
+                '{{ guard }}',
+                "!auth()->guest()",
+                file_get_contents(app_path('Providers/TelescopeServiceProvider.php'))
+            ));
+        }
     }
 }
